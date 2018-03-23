1fbfacb897a332b3255f903c162ae2576ac2b94a
Ralph Cacho <cacho.rc@gmail.com>
2018-03-23 11:07:16 -0700

    Countdown: Add countdown elements and ability to change the deadline title.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline() {
+        this.setState({deadline: this.state.newDeadline})
+    }
+
     render() {
         return (
-            <div>Countdown Champ, App Component</div>
+            <div className="App">
+                <div className="App-title">
+                    Countdown to {this.state.deadline}
+                </div>
+                <div>
+                    <div className="Clock-days">100 days</div>
+                    <div className="Clock-days">21 hours</div>
+                    <div className="Clock-days">40 minutes</div>
+                    <div className="Clock-days">2 seconds</div>
+                </div>
+                <div>
+                    <input
+                        placeholder='new date'
+                        onChange={event => this.setState({newDeadline: event.target.value})}
+                    />
+                    <button onClick={() => this.changeDeadline()}>
+                        Submit
+                    </button>
+                </div>
+            </div>
         )
     }
 }

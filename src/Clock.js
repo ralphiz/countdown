ee7f23c2b13667748a05cad2f128cc52ea343c88
Ralph Cacho <cacho.rc@gmail.com>
2018-03-23 11:50:45 -0700

    Implement countdown functionality

--- src/Clock.js
+++ src/Clock.js
@@ -10,7 +10,24 @@ class Clock extends Component {
             minutes: 0,
             seconds: 0
         }
-        console.log('this.props', this.props)
+    }
+
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline)
+    }
+
+    componentDidMount() {
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
+    }
+
+    getTimeUntil(deadline) {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours = Math.floor(time/(1000*60*60) & 24);
+        const days = Math.floor(time/(1000*60*60*24));
+
+        this.setState({days, hours, minutes, seconds})
     }
 
     render() {

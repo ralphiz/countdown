045b14c094975e2314b960124477773c499f05a9
Ralph Cacho <cacho.rc@gmail.com>
2018-03-25 21:30:30 -0700

    Add TODO

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
+// @TODO:
+// 1. Stopwatch component
+// 2. Add alert when timer reaches 0
+// 3. Host
 ReactDom.render(
     <App/>,
     document.getElementById('root')

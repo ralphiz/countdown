922f072f29f3b25589b87ff6771414b6a81629e0
Ralph Cacho <cacho.rc@gmail.com>
2018-04-11 11:17:24 -0700

    Add react-router-dom for Github Pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // @TODO:
 // 1. Stopwatch component
 // 2. Add alert when timer reaches 0
 // 3. Host
 ReactDom.render(
-    <App/>,
-    document.getElementById('root')
+  <Router basename={process.env.PUBLIC_URL}>
+    <App/>
+  </Router>,
+  document.getElementById('root')
 );
